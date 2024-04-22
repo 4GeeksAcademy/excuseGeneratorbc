b659b95282f21a819c2a20e234766c4d9eebd13e
Eduardo-correa1 <159133054+Eduardo-correa1@users.noreply.github.com>
2024-04-22 18:33:35 +0000

    r

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#La-excusa").innerHTML = randomNumber;
+  console.log("hola");
+};
+let generateExcuse = () => {
+  return "My dog eat my homework";
 };
