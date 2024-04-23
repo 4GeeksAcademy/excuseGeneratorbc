eb49b8e1719b419e0fdb32888b535348b0ba60c1
Eduardo-correa1 <159133054+Eduardo-correa1@users.noreply.github.com>
2024-04-23 03:25:29 +0000

    fin

--- src/app.js
+++ src/app.js
@@ -7,10 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   //write your code here
-  let randomNumber = Math.random() * 10;
-  document.querySelector("#La-excusa").innerHTML = randomNumber;
-  console.log("hola");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#La-excusa").innerHTML = generateExcuse();
+  });
 };
 let generateExcuse = () => {
-  return "My dog eat my homework";
+  let pron = ["My", "Your", "The"];
+  let who = ["grandmother", "dog", "President", "Alien"];
+  let act = ["Burned down", "caused chaos on", "flooded", "ripped", "ate"];
+  let thin = ["my head", "my house", "my homework", "my country", "my family"];
+  let how = ["dancing reggaeton", "while eating an ice cream", "accidentally"];
+
+  let pronind = Math.floor(Math.random() * pron.length);
+  let whoind = Math.floor(Math.random() * who.length);
+  let actind = Math.floor(Math.random() * act.length);
+  let thinind = Math.floor(Math.random() * thin.length);
+  let howind = Math.floor(Math.random() * how.length);
+  return (
+    pron[pronind] +
+    " " +
+    who[whoind] +
+    " " +
+    act[actind] +
+    " " +
+    thin[thinind] +
+    " " +
+    how[howind]
+  );
 };
